1359a52a65e03b3e3e900b69bd972e15ecda5cb9
Mikhail <mikhail-zmyslin@yandex.ru>
2022-07-12 17:27:59 +0300

    решение 1 задачи

---
+++ task001_seminar3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write($"Введите число: ");
+string number = Console.ReadLine();
+
+void Palindrom(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"Ваше число: {number} -  это палиндром.");
+    }
+    else Console.WriteLine($"Ваше число: {number} - это не палиндром.");
+}
+
+if (number!.Length == 5)
+{
+    Palindrom(number);
+}
+else Console.WriteLine($"Введи правильное число");
