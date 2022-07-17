4ae724d5e190671dcc98399d0f4f7af591f0ab6f
Mikhail <mikhail-zmyslin@yandex.ru>
2022-07-17 20:13:33 +0300

    решение 3 задачи

---
+++ task003_seminar3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+Console.WriteLine($"Эта программа принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.");
+
+Console.WriteLine("Введите число больше 1 : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int thirdDegree(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write(i * i * i + " | ");
+    }
+    return n;
+}
+thirdDegree(number);
